b39b172cc01ca18191ef08973c931053c34be35b
Shlok <s2shlok@gmail.com>
2019-08-29 18:31:00 +0530

    test

---
+++ leetcode/arrays/test.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    vector<int> pancakeSort(vector<int>& A) {
+        
+        vector<int> ans;
+        int n = A.size();
+        while(n > 1){
+        int maxIndex = check(A,n);
+        ans.push_back(maxIndex);
+        reverse(A.begin(),A.begin() + maxIndex); 
+        reverse(A.begin(),A.begin() + n);
+        ans.push_back(n);
+        n--;
+        }
+        return ans;
+            
+    }
+    
+    int check(vector<int> a, int n){
+        int m = -1;
+        int ind;
+        for(int i =0; i < n; i++){
+            if(m < a[i]){
+                m = a[i];
+                ind = i;
+            }
+        }
+        return ind + 1 ;
+    }
+};
+
+
+
+
+
