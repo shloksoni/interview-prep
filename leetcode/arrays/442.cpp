239007a93b3b90cdba490b99798c9f5f43606f9a
Shlok <s2shlok@gmail.com>
2019-08-29 17:34:47 +0530

    Duplicates in the array in O(n)

--- leetcode/arrays/442.cpp
+++ leetcode/arrays/442.cpp
@@ -1,33 +1,13 @@
-#include<bits/stdc++.h>
-#define vi vector<int>
-#define ll long long
-#define REP( i,  b,  n) for(int i =b; i< n; i++)
-using namespace std;
-map<string,int>mp;
-
-int gcd(int a, int b){
-    if(b == 0)return a;
-    return gcd(b, a % b);
-}
-void solve(){
-   double a,b;
-   cin>>a>>b;
-   double ans = a  * b;
-   cout<<fixed;
-  cout << std::setprecision(6) << (a > 1000 ? ans * 0.9 : ans) << '\n';    
-    
-    
-}
-
-int main(){
-   
-    int t;
-    cin>>t;
-    while(t--)solve();
-
-    return 0;
-
-   
-
-
-}
+class Solution {
+public:
+    vector<int> findDuplicates(vector<int>& nums) {
+      vector<int> ans;
+        
+        for(int i =0; i <nums.size(); i++){
+            if(nums[abs(nums[i]) - 1] > 0)  nums[abs(nums[i])- 1] *= -1;
+            else   ans.push_back(abs(nums[i]));
+              
+        }
+        return ans;
+    }
+};
